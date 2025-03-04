59dcc811ba351017002bc2b4c791ee21f1b6da59
aida <aidasagan369@gmail.com>
2025-03-04 10:08:59 +0500

    updated

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,27 @@
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/";
+dotenv.config();
 
-const connectDB = async () => {
+const MONGODB_URI =
+    process.env.NODE_ENV === "development"
+        ? process.env.MONGODB_URI_LOCAL
+        : process.env.MONGODB_URI_ATLAS;
+
+if (!MONGODB_URI) {
+    throw new Error("❌ MONGODB_URI не задана! Добавь её в .env или Vercel.");
+}
+
+const connectDB = async (): Promise<void> => {
     if (mongoose.connection.readyState >= 1) return;
+
     try {
-        await mongoose.connect(MONGODB_URI);
-        console.log("✅ Подключено к MongoDB");
+        await mongoose.connect(MONGODB_URI, {});
+        console.log("✅ Подключено к MongoDB:", process.env.NODE_ENV);
     } catch (error) {
         console.error("❌ Ошибка подключения к MongoDB:", error);
+        process.exit(1);
     }
-
 };
 
 export default connectDB;
